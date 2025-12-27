e7ba0e37c5fce451a11ba24afed583a593bced1c
bururun <bururun@users.noreply.github.com>
2025-12-28 01:26:35 +0200

    fix: add error messages

--- lib/helpers.py
+++ lib/helpers.py
@@ -29,3 +29,14 @@ def helper_function_18(x):
 def format_output(data):
     """Format output data."""
     return str(data).upper()
+
+
+# Helper functions
+
+def helper_function_19(x):
+    """Helper function for iteration 19."""
+    return x * 19
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
